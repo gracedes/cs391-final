61a93402228edd555e909d0323bba856d530de0e
Edward Reyna <reynaedward108@gmail.com>
2026-04-04 20:53:28 -0400

    Created getAllPosts function for mongodb

---
+++ lib/getAllPosts.ts
@@ -0,0 +1,26 @@
+
+import {PostProps} from "@/lib/PostProps";
+import getCollection, {POSTS_COLLECTION} from "@/lib/db";
+
+export default async function getAllPosts():Promise<PostProps[]>{
+
+    const postsCollection=await getCollection(POSTS_COLLECTION);
+    const data=await postsCollection.find().toArray();
+
+    const posts:PostProps[]=data.map((p)=>
+        (
+            {
+                id: p._id.toHexString(),
+                title: p.title,
+                username: p.username,
+                tags: p.tags,
+                content:p.content,
+                image: p.image,
+                upvotes:p.upvotes,
+                downvotes:p.downvotes,
+            }
+        )
+    )
+
+    return posts.reverse();
+}
