7ac35209452e3e6f4ceb271a19dc2fd6cb11192b
Edward Reyna <reynaedward108@gmail.com>
2026-04-07 21:50:50 -0400

    Added pages for Home and Following Page

---
+++ app/following/page.tsx
@@ -0,0 +1,10 @@
+import PostsDisplay from "@/app/components/PostDisplay";
+import getFollowingPosts from "@/lib/getFollowingPosts";
+
+export default async function FollowingPage(){
+    const followingPosts = await getFollowingPosts("edward");
+
+    return(
+        <PostsDisplay inputPosts={followingPosts}/>
+    );
+}
