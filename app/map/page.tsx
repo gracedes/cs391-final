daf1f4a26dc9b464f6cb756ca803e6c303816b95
Edward Reyna <reynaedward108@gmail.com>
2026-04-11 14:39:24 -0400

    Implemented Map page with posts from Mongodb

---
+++ app/map/page.tsx
@@ -0,0 +1,35 @@
+import { PostProps } from "@/app/interfaces/PostProps";
+import BlogMap from "@/app/components/BlogMap";
+import styled from "styled-components";
+import getAllPosts from "@/lib/getAllPosts";
+
+const PageMain = styled.main`
+  padding: 2rem;
+  max-width: 1200px;
+  margin: 0 auto;
+`;
+
+const PageHeader = styled.h1`
+  margin-bottom: 1rem;
+  font-size: 2rem;
+  font-weight: bold;
+`;
+
+const PageDescription = styled.p`
+  margin-bottom: 2rem;
+  color: #555;
+`;
+
+export default async function MapPage() {
+    const posts = await getAllPosts();
+
+    return (
+        <PageMain>
+            <PageHeader>Global Blog Explorer</PageHeader>
+            <PageDescription>
+                Zoom out to see grouped posts, or zoom in to see individual pins!
+            </PageDescription>
+            <BlogMap posts={posts} />
+        </PageMain>
+    );
+}
