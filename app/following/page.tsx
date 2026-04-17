cdf1873c1ff7769252dcdf464b94cef50951c9b8
lizlam8 <lizlam@bu.edu>
2026-04-17 12:02:32 -0400

    yay commit for new post creation! (#17)

--- app/following/page.tsx
+++ app/following/page.tsx
@@ -20,6 +20,7 @@ export default async function FollowingPage(){
 
     const followingPosts = await getFollowingPosts();
 
+
     return(
         <PostsDisplay inputPosts={followingPosts}/>
     );
---
+++ lib/makeBlogPost.ts
@@ -0,0 +1,26 @@
+"use server";
+
+import { PostProps } from "@/app/interfaces/PostProps";
+import getCollection, {POSTS_COLLECTION} from "@/lib/db";
+
+
+export default async function makeBlogPost(title:string, content:string, tags: string[], username: string):Promise<PostProps | null>{
+    const p ={
+        title: title,
+        username: username,
+        content: content,
+        tags: tags,
+        upvotes: 0,
+        downvotes: 0,
+    }
+
+    const postsCollection = await getCollection(POSTS_COLLECTION);
+    const res = await postsCollection.insertOne(p);
+
+    if(!res.acknowledged){
+        return null;
+    }
+
+
+    return {...p, id: res.insertedId.toHexString()};
+}
