ced4c8af04ce01413024a9bf877b713f5d3f4fee
Edward Reyna <reynaedward108@gmail.com>
2026-04-19 13:18:32 -0400

    Updated UserInfo to use useEffect to fetch username

--- app/components/profile/UserInfo.tsx
+++ app/components/profile/UserInfo.tsx
@@ -1,9 +1,21 @@
-"use server";
+"use client";
 
 import getProfile from "@/lib/getProfile";
+import {useEffect, useState} from "react";
+import {UserProps} from "@/app/interfaces/UserProps";
 
-export default async function UserInfo(username:string) {
-    const profile = await getProfile(username);
+export default function UserInfo({ username }: { username: string }) {
+    const [profile, setProfile] = useState<UserProps | null>(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        getProfile(username).then((data) => {
+            setProfile(data);
+            setLoading(false);
+        });
+    }, [username]);
+
+    if (loading) return <p>Loading...</p>;
 
     if (!profile) {
         return (<p>No profile found at that username :(</p>)
