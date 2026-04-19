c7e19bfdd97d714cb65550d60fb622d7de8ffeba
Edward Reyna <reynaedward108@gmail.com>
2026-04-19 14:11:32 -0400

    Updated makeBlogPost to check for longitude and latitude

--- lib/makeBlogPost.ts
+++ lib/makeBlogPost.ts
@@ -4,7 +4,7 @@ import { PostProps } from "@/app/interfaces/PostProps";
 import getCollection, {POSTS_COLLECTION} from "@/lib/db";
 
 
-export default async function makeBlogPost(title:string, content:string, tags: string[], username: string):Promise<PostProps | null>{
+export default async function makeBlogPost(title:string, content:string, tags: string[], username: string, longitude?: number | null, latitude?: number | null):Promise<PostProps | null>{
     const p ={
         title: title,
         username: username,
@@ -12,7 +12,9 @@ export default async function makeBlogPost(title:string, content:string, tags: s
         tags: tags,
         upvotes: 0,
         downvotes: 0,
-    }
+        ...(longitude !== undefined && { longitude }),
+        ...(latitude !== undefined && { latitude }),
+    };
 
     const postsCollection = await getCollection(POSTS_COLLECTION);
     const res = await postsCollection.insertOne(p);
