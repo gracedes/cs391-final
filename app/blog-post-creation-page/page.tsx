ede1477799bcdca3cb86be9cf690de200862433f
Edward Reyna <reynaedward108@gmail.com>
2026-04-19 14:13:02 -0400

    Added login check to create blog post

--- app/blog-post-creation-page/page.tsx
+++ app/blog-post-creation-page/page.tsx
@@ -1,6 +1,22 @@
 import NewPostForm from "@/app/components/NewPostForm";
+import {auth} from "@/lib/auth";
+import {headers} from "next/headers";
+import {redirect} from "next/navigation";
 
 export default async function BlogPostCreationPage(){
-    return (<NewPostForm/>);
+    const session = await auth.api.getSession({
+        headers: await headers()
+    });
+
+    if(!session || !session.user){
+        redirect("/login")
+    }
+
+    const currentUser = session.user as any;
+    if (!currentUser.username || currentUser.username.trim() === ""){
+        redirect("/setup-username");
+    }
+
+    return (<NewPostForm username={currentUser.username}/>);
 };
 
