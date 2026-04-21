b75e9164f6fb3436134a604b3f01835aaef9be37
Edward Reyna <156349623+EdRey24@users.noreply.github.com>
2026-04-21 17:54:59 -0400

    Dynamic Navbar (#32)
    * remove unnecessary files
    
    * remove unnecessary files
    
    * basic flexbox setup
    
    * static navbar looks good, just need to add changed on interaction
    
    * forgot to add images to previous commit
    
    * font scaling
    
    * gitignore
    
    * ignore .idea folder
    
    * rearrange files to adjust for new folder structure in main
    
    * Delete package.json
    
    * Remove .idea directory from .gitignore
    
    Remove ignored directory for IDE configuration.
    
    * Delete README.md
    
    * remove empty/unneededfiles
    
    * note to self
    
    * wordmark logo in corner of navbar
    
    * not yet working dynamic navbar
    
    * Updated Nav Bar styling to show a border around current page
    
    * Added ring around profile image when on profile page
    
    ---------
    
    Co-authored-by: grace <gracedes@bu.edu>

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,16 +1,17 @@
 "use client"
 
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import Link from "next/link";
 import {authClient} from "@/lib/auth-client";
 import ProfileDropdown from "@/app/components/ProfileDropdown";
+import {usePathname} from "next/navigation";
 
 // TODO: add new post button?
 
 const NavBar = styled.nav`
     display: flex;
     flex-direction: row;
-    justify-content: center;
+    justify-content: space-evenly;
     align-items: center;
     background-color: #232C33;
     height: 10vh;
@@ -18,15 +19,47 @@ const NavBar = styled.nav`
     box-sizing: border-box;
     padding: 0 4vw;
     font-size: calc(14px + 0.75vw);
-    .text-link {
-        text-decoration: none;
-        color: white;
-        margin: 0 4vw;
-    }
-    .center-links {
-        margin: 0 auto;
-        width: fit-content;
-    }
+`;
+
+const LinksDiv = styled.div`
+    display: flex;
+    margin: 0 auto;
+    width: 50vw;
+    justify-content: space-evenly;
+`;
+
+const StyledLinks = styled(Link)`
+    color: white;
+    text-decoration: none;
+    margin-left: 2vw;
+    font-size: calc(2px + 3vw);
+    text-align: center;
+    padding: 0 1vw;
+    width: fit-content;
+`;
+
+const DiscoverLink = styled(StyledLinks)<{ $page: string }>`
+    ${props => props.$page === '/' && css`
+        background-color: white;
+        color: black;
+        border-radius: 40vw;
+    `}
+`;
+
+const FollowingLink = styled(StyledLinks)<{ $page: string }>`
+    ${props => props.$page === '/following' && css`
+        background-color: white;
+        color: black;
+        border-radius: 40vw;
+    `}
+`;
+
+const MapLink = styled(StyledLinks)<{ $page: string }>`
+    ${props => props.$page === '/map' && css`
+        background-color: white;
+        color: black;
+        border-radius: 40vw;
+    `}
 `;
 
 const Wordmark = styled.h1`
@@ -41,14 +74,16 @@ const Wordmark = styled.h1`
 export default function Nav() {
     const { data: session, isPending } = authClient.useSession();
 
+    const pathname = usePathname()
+
     return (
         <NavBar>
             <Wordmark>Revival</Wordmark>
-            <div className="center-links">
-                <Link className="text-link" href="/">Discover</Link>
-                <Link className="text-link" href="/following">Following</Link>
-                <Link className="text-link" href="/map">Map</Link>
-            </div>
+            <LinksDiv>
+                <DiscoverLink href="/" $page={pathname}>Discover</DiscoverLink>
+                <FollowingLink href="/following" $page={pathname}>Following</FollowingLink>
+                <MapLink href="/map" $page={pathname}>Map</MapLink>
+            </LinksDiv>
             <ProfileDropdown session={session} isPending={isPending} imageSize="7vh"/>
         </NavBar>
     );
